791d66a7f20e23bf887d8fe5b3fce6c88faac9c6
NewBalanse <coocies12@gmail.com>
2020-11-16 15:25:53 +0200

    add route get post by category

--- routes/home/home.router.ts
+++ routes/home/home.router.ts
@@ -24,8 +24,20 @@ homePageRouter.get('/all-categories', async (req, res) => {
     const connection = await db();
     const categoriesModel = mongoose.model('category', categoriesSchema);
 
-    categoriesModel.find({}, (err, result: CategoriesInterface) => {
+    categoriesModel.find({}, (err, result: CategoriesInterface[]) => {
         connection.disconnect();
+        if (err) return res.status(400).json(err);
+
+        return res.status(200).json(result);
+    })
+})
+
+homePageRouter.get('post-by-category/:id', async (req, res) => {
+    const connections = await db();
+    const postModel = mongoose.model('post', postSchema);
+
+    postModel.find({categoryId: req.params.id}, (err, result: PostInterface[]) => {
+        connections.disconnect();
         if (err) return res.status(400).json(err);
 
         return res.status(200).json(result);
